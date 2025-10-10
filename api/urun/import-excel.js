b04225e0404acfafc1b7d133efeeb96386437f75
VMS Developer <vms@example.com>
2025-10-10 16:29:39 +0300

    Serverless function sayÄ±sÄ± 12 limit altÄ±na dÃ¼ÅŸÃ¼rÃ¼ldÃ¼ - sadece 5 temel endpoint kaldÄ±

--- api/urun/import-excel.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res) => {
-  // Vercel'de Excel import simülasyonu
-  res.json({
-    success: true,
-    message: 'Excel dosyası başarıyla yüklendi',
-    imported: 10,
-    errors: 0
-  });
-};
